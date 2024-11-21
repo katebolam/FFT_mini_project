317355d6d19cfe18abd7f56360a7feaf2388d391
katebolam <katebolam.x@icloud.com>
2024-11-21 17:19:59 +0000

    feat: argparse of number of processes

--- multiprocessing/1D_multiprocessing_FFT.py
+++ multiprocessing/1D_multiprocessing_FFT.py
@@ -1,10 +1,8 @@
-# This script implements a 1D fast Fourier transform (FFT) for a process-parallelized case using the multiprocessing Python module,
-# and stores and plots the execution times as a function of problem size (N)
-
+import argparse
+from multiprocessing import Pool
 import numpy as np
 import sys
 import os
-from multiprocessing import Pool
 
 # Add utils directory to the path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'utils')))
@@ -12,7 +10,7 @@ from timing import time_function
 from plotting import plot_execution_times
 
 # Define the problem sizes as powers of 2 from 2^0 to 2^12
-problem_sizes = [2**i for i in range(13)] 
+problem_sizes = [2**i for i in range(13)]
 execution_times = []  # To store mean execution times for each problem size
 
 # Number of runs for averaging
@@ -22,28 +20,30 @@ num_runs = 1
 def time_fft(data):
     return time_function(np.fft.fft, data)
 
-# Main block to run the code with multiprocessing
+# Main block to run the code with argparse
 if __name__ == "__main__":
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description="1D FFT using Multiprocessing")
+    parser.add_argument(
+        "--processes",
+        type=int,
+        default=4,
+        help="Number of processes to use (default: 4)"
+    )
+    args = parser.parse_args()
+
+    num_processes = args.processes  # Get number of processes from command-line arguments
+
     # Run the 1D FFT with multiprocessing for each problem size
     for size in problem_sizes:
         data = np.random.random(size)  # Generate random 1D data for FFT
 
         # Use a pool of worker processes to parallelize timing runs
-        with Pool() as pool:
+        with Pool(processes=num_processes) as pool:  # Use argument for number of processes
             # Run `time_fft` across `num_runs` parallel executions
             times = pool.map(time_fft, [data] * num_runs)
 
         mean_time = np.mean(times)  # Calculate mean execution time across runs
         execution_times.append(mean_time)
 
-        print(f"1D (Multiprocessing) FFT Execution Time for N={size}: {mean_time:.10f} seconds")
-
-    # Plot the execution times with problem size N as x-axis
-    plot_execution_times(
-        problem_sizes,
-        [execution_times],  
-        labels=["Multiprocessing 1D FFT"], 
-        title="1D FFT Multiprocessing Execution Time vs. Problem Size",
-        xlabel="Problem Size (2^N)",
-        ylabel="Execution Time (s)"
-    )
+        print(f"1D (Multiprocessing) FFT Execution Time for N={size}, Processes={num_processes}: {mean_time:.10f} seconds")
--- multiprocessing/2D_multiprocessing_FFT.py
+++ multiprocessing/2D_multiprocessing_FFT.py
@@ -1,6 +1,4 @@
-# This script implements a 2D fast Fourier transform (FFT) for a process-parallelized case using the multiprocessing Python module,
-# and stores and plots the execution times as a function of problem size (N)
-
+import argparse
 import numpy as np
 import sys
 import os
@@ -12,7 +10,7 @@ from timing import time_function
 from plotting import plot_execution_times
 
 # Define the problem sizes as powers of 2 for square matrices from 2^0 to 2^12
-problem_sizes = [2**i for i in range(13)]  
+problem_sizes = [2**i for i in range(13)]
 execution_times = []  # To store mean execution times for each problem size
 
 # Number of runs for averaging
@@ -22,28 +20,30 @@ num_runs = 1
 def time_fft2(data):
     return time_function(np.fft.fft2, data)
 
-# Main block to run the code with multiprocessing
+# Main block to run the code with argparse
 if __name__ == "__main__":
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description="2D FFT using Multiprocessing")
+    parser.add_argument(
+        "--processes",
+        type=int,
+        default=4,
+        help="Number of processes to use (default: 4)"
+    )
+    args = parser.parse_args()
+
+    num_processes = args.processes  # Get number of processes from command-line arguments
+
     # Run the 2D FFT with multiprocessing for each problem size
     for size in problem_sizes:
         data = np.random.random((size, size))  # Generate random 2D data for FFT
 
         # Use a pool of worker processes to parallelize timing runs
-        with Pool() as pool:
+        with Pool(processes=num_processes) as pool:  # Use argument for number of processes
             # Run `time_fft2` across `num_runs` parallel executions
             times = pool.map(time_fft2, [data] * num_runs)
 
         mean_time = np.mean(times)  # Calculate mean execution time across runs
         execution_times.append(mean_time)
 
-        print(f"2D (Multiprocessing) FFT Execution Time for {size}x{size}: {mean_time:.10f} seconds")
-
-    # Plot the execution times with problem size N as x-axis
-    plot_execution_times(
-        problem_sizes,
-        [execution_times],  
-        labels=["Multiprocessing 2D FFT"], 
-        title="2D FFT Multiprocessing Execution Time vs. Problem Size",
-        xlabel="Problem Size (2^N x 2^N)",
-        ylabel="Execution Time (s)"
-    )
+        print(f"2D (Multiprocessing) FFT Execution Time for {size}x{size}, Processes={num_processes}: {mean_time:.10f} seconds")
